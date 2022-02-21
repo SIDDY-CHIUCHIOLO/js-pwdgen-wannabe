3142447bc4c4c59f2caf47f0f8625425538c5248
siddhartha chiuchiolo <siddychiuchiolo7@gmail.com>
2022-02-21 18:10:18 +0100

    modificato lae variabili let in const nel file js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 
-let firstName = prompt("What is your name?")
+const firstName = prompt("What is your name?")
 document.getElementById("my-name").innerHTML = firstName;
 
-let lastName = prompt("What is your last name?")
+const lastName = prompt("What is your last name?")
 document.getElementById("my-last-name").innerHTML = lastName
 
-let favoriteColor = prompt("What is your favourite colour?")
+
+const favoriteColor = prompt("What is your favourite colour?")
 document.getElementById("my-favorite-color").innerHTML = favoriteColor
 
-let password = firstName + lastName + favoriteColor;
+
+const password = firstName + lastName + favoriteColor;
 console.log(password);
 document.getElementById("my-password").innerHTML = password
