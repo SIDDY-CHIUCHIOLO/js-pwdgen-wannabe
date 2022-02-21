1c5164fed79e9637d0c3d758307f42d5f9a2916d
siddhartha chiuchiolo <siddychiuchiolo7@gmail.com>
2022-02-21 16:20:54 +0100

    inserito le tre domande

---
+++ js/main.js
@@ -0,0 +1,9 @@
+
+const firstName = prompt("What is your name?")
+document.getElementById("my-name").innerHTML = firstName;
+
+const lastName = prompt("What is your last name?")
+document.getElementById("my-last-name").innerHTML = lastName
+
+const favoriteColor = prompt("What is your favourite colour?")
+document.getElementById("my-favorite-color").innerHTML = favoriteColor
