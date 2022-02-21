a2aee6c8380ce5a7ecc476667cf07c2779ca1f42
siddhartha chiuchiolo <siddychiuchiolo7@gmail.com>
2022-02-21 17:41:41 +0100

    inserito password generator

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 
-const firstName = prompt("What is your name?")
+let firstName = prompt("What is your name?")
 document.getElementById("my-name").innerHTML = firstName;
 
-const lastName = prompt("What is your last name?")
+let lastName = prompt("What is your last name?")
 document.getElementById("my-last-name").innerHTML = lastName
 
-const favoriteColor = prompt("What is your favourite colour?")
+let favoriteColor = prompt("What is your favourite colour?")
 document.getElementById("my-favorite-color").innerHTML = favoriteColor
+
+let password = firstName + lastName + favoriteColor;
+console.log(password);
+document.getElementById("my-password").innerHTML = password
